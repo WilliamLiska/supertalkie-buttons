a0b202fe6569d9488885d253c3a4ff042b72ad8c
williamliska <william.liska@gmail.com>
2017-07-06 03:50:27 +0000

    Clean up unused buttons.  Add volume buttons and their callbacks.

--- supertalkie-buttons.py
+++ supertalkie-buttons.py
@@ -5,11 +5,12 @@ import RPi.GPIO as GPIO
 import subprocess
 GPIO.setmode(GPIO.BCM)
 
-# GPIO 17 & 5 set up as inputs, pulled up to avoid false detection.
+# GPIOs set up as inputs, pulled up to avoid false detection.
 # Both ports are wired to connect to GND on button press.
 # So we'll be setting up falling edge detection for both
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(27, GPOIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(22, GPOIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # GPIO 26 set up as an input, pulled down, connected to 3V3 on button press
 GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -18,37 +19,40 @@ GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(16, GPIO.OUT)
 GPIO.output(16, GPIO.LOW)
 
-# now we'll define two threaded callback functions
+# now we'll define the threaded callback functions
 # these will run in another thread when our events are detected
-def my_callback(channel):
-    print "falling edge detected on 5"
 
-def my_callback2(channel):
+def wifi_reset_callback(channel):
     print "falling edge detected on 17"
     GPIO.output(16, GPIO.HIGH)
     subprocess.Popen("sudo node server.js ForceChange", cwd='/home/mumble/raspberry-wifi-conf', shell=True)
     GPIO.output(16, GPIO.LOW)
 
-print "Make sure you have a button connected so that when pressed"
-print "it will connect GPIO port 17 (pin 11) to GND (pin 6)\n"
-print "You will also need a second button connected so that when pressed"
-print "it will connect GPIO port 26 (pin 37) to 3V3 (pin 1)\n"
-print "You will also need a third button connected so that when pressed"
-print "it will connect GPIO port 5 (pin 29) to GND (pin 14)"
+def volume_up_callback(channel):
+    print "falling edge detected on 27"
+    GPIO.output(16, GPIO.HIGH)
+    #make volume go up
+    subprocess.Popen("amixer sset Master 10%+" shell=True)
+    GPIO.output(16, GPIO.LOW)
 
-# when a falling edge is detected on port 5, regardless of whatever
-# else is happening in the program, the function my_callback will be run
-GPIO.add_event_detect(5, GPIO.FALLING, callback=my_callback, bouncetime=300)
+def volume_down_callback(channel):
+    print "falling edge detected on 22"
+    GPIO.output(16, GPIO.HIGH)
+    #make volume go down
+    subprocess.Popen("amixer sset Master 10%-" shell=True)
+    GPIO.output(16, GPIO.LOW)
 
-# when a falling edge is detected on port 17, regardless of whatever
-# else is happening in the program, the function my_callback2 will be run
+# when a falling edge is detected on an input port, regardless of whatever
+# else is happening in the program, the function wifi_reset_callback will be run
 # 'bouncetime=300' includes the bounce control written into interrupts2a.py
-GPIO.add_event_detect(17, GPIO.FALLING, callback=my_callback2, bouncetime=300)
+GPIO.add_event_detect(17, GPIO.FALLING, callback=wifi_reset_callback, bouncetime=300)
+GPIO.add_event_detect(27, GPIO.FALLING, callback=volume_up_callback, bouncetime=300)
+GPIO.add_event_detect(22, GPIO.FALLING, callback=volume_down_callback, bouncetime=300)
 
 try:
     print "Waiting for rising edge on port 26"
     GPIO.wait_for_edge(26, GPIO.RISING)
-    print "Rising edge detected on port 26. Here endeth the third lesson."
+    print "Rising edge detected on port 26. Exiting."
 
 except KeyboardInterrupt:
     GPIO.cleanup()       # clean up GPIO on CTRL+C exit
